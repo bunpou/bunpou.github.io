ec791682d18d9a9140a7f3447d2eefa6a0c1338b
edikxl <edik24032002@gmail.com>
2021-01-06 14:16:07 +0200

    Smallest working SPA is done. However rendering inner page html is still broken :c

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Awesome Japanese
 
-[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
-[![HTML Hint](https://img.shields.io/badge/code_style-htmlhint-brightgreen.svg)](https://htmlhint.com)
-[![CSS Lint](https://img.shields.io/badge/code_style-csslint-brightgreen.svg)](https://github.com/CSSLint/csslint)
+[![HTML Hint](https://img.shields.io/badge/HTML_code_style-htmlhint-brightgreen.svg)](https://htmlhint.com)
+[![CSS Lint](https://img.shields.io/badge/CSS_code_style-csslint-brightgreen.svg)](https://github.com/CSSLint/csslint)
+[![JavaScript Style Guide](https://img.shields.io/badge/JS_code_style-standard-brightgreen.svg)](https://standardjs.com)
--- app.js
+++ app.js
@@ -1,42 +1,50 @@
 import { LitElement, html } from 'https://unpkg.com/lit-element@2.1.0/lit-element.js?module'
 import Navigo from 'https://unpkg.com/navigo@7.1.2/lib/navigo.es.js'
-/* global customElements, fetch */
+/* global customElements, DOMParser, fetch */
 
 class AppPage extends LitElement {
   static get properties () {
-    return { page: { type: Object } }
+    return { innerHTML: { type: Object } }
   }
 
   constructor () {
     super()
 
-    const root = '/' // Change later if bugs occure
+    this.parser = new DOMParser()
+
+    const root = '/'
     const useHash = true
     const hash = '#'
     const router = new Navigo(root, useHash, hash)
 
     router
+      .on('*', () => {
+        const URL = './pages' + document.location.pathname + '/page.html'
+        this.loadPage(URL)
+      })
+      /*
       .on('/home', () => {
-        this.page = html`<div>HOME TEST</div>` // this.loadPage('home')
+        this.innerHTML = html`<div>HOME TEST</div>` // this.loadPage('./pages/home/page.html')
       })
       .on('/reader', () => {
-        this.page = html`<div>READER TEST</div>` // this.loadPage('reader')
+        this.innerHTML = html`<div>READER TEST</div>` // this.loadPage('./pages/reader/page.html')
       })
-      .on('*', () => { // Just send to a url that is '/pages/' + previousURL in ALL cases, not only '*'
-        this.page = html`<div>ANY UNKNOWN PAGE TEST</div>` // this.loadPage('404')
-      })
+      */
 
     router.resolve()
   }
 
   render () {
-    return html`<div>${this.page}</div>`
+    return html`<div>${this.innerHTML}</div>` // TODO: Check what render should return to render this correctly
   }
 
-  async loadPage (pagePath) {
-    const response = await fetch(`./pages/${pagePath}/index.html`)
-    const text = await response.text()
-    return text
+  loadPage (URL) {
+    fetch(URL).then(response => {
+      response.text().then(text => {
+        this.innerHTML = this.parser.parseFromString(text, 'text/html')
+        console.log(this.innerHTML)
+      })
+    })
   }
 }
 
