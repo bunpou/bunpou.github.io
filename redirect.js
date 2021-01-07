fdb9a9f60eaae6747ce06d22560e4211c985aa53
edikxl <edik24032002@gmail.com>
2021-01-07 17:30:28 +0200

    Improved code style for better readability

--- redirect.js
+++ redirect.js
@@ -1,10 +1,13 @@
-(function(l) {
-  if (l.search[1] === '/' ) {
-    var decoded = l.search.slice(1).split('&').map(function(s) { 
+(function (l) {
+  console.log(l)
+  if (l.search[1] === '/') {
+    const decoded = l.search.slice(1).split('&').map(function (s) {
       return s.replace(/~and~/g, '&')
-    }).join('?');
-    window.history.replaceState(null, null,
-        l.pathname.slice(0, -1) + decoded + l.hash
-    );
+    }).join('?')
+    window.history.replaceState(
+      null,
+      null,
+      l.pathname.slice(0, -1) + decoded + l.hash
+    )
   }
 }(window.location))
