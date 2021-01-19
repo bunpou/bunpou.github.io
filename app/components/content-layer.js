96b70fa90775e5244e20df3cdac262de76b614ba
edikxl <edik24032002@gmail.com>
2021-01-19 18:45:49 +0200

    Going from LitElement to Vanilla JS Web Components

--- app/components/content-layer.js
+++ app/components/content-layer.js
@@ -1,51 +1,16 @@
-import { LitElement, html } from 'https://unpkg.com/lit-element@2.1.0/lit-element.js?module'
-import { unsafeHTML } from 'https://unpkg.com/lit-html@1.3.0/directives/unsafe-html.js?module'
-import Navigo from 'https://unpkg.com/navigo@7.1.2/lib/navigo.es.js'
-/* global customElements, fetch */
+/* global customElements, fetch, HTMLElement */
 
-class ContentLayer extends LitElement {
-  static get properties () {
-    return { innerHTML: { type: String } }
-  }
-
-  constructor () {
-    super()
-
-    const root = '/'
-    const useHash = false
-    const defaultPage = '/home'
-
-    // TODO: Move router from content-layer to index.html and make it global
-    this.router = new Navigo(root, useHash)
-
-    this.router
-      .on('*', () => {
-        let pathname = document.location.pathname
-
-        if (pathname === '/') {
-          pathname = defaultPage
-        }
-
-        const URL = './pages' + pathname + '/page.html'
-
-        this.loadPage(URL, defaultPage)
-      })
-      .resolve()
-  }
-
-  render () {
-    return html`<div>${this.innerHTML}</div>`
-  }
-
-  loadPage (URL, defaultPage = '/index.html') {
+class ContentLayer extends HTMLElement {
+  changePageTo (URL, defaultPage = '/index.html') {
     fetch(URL)
       .then(response => {
         if (response.ok) {
           response.text().then(text => {
-            this.innerHTML = html`${unsafeHTML(text)}`
+            this.innerHTML = text
           })
         } else if (URL !== defaultPage) {
-          this.router.navigate(defaultPage.slice(1)) // '/home' -> 'home', otherwise error
+          document.router.navigate(defaultPage.slice(1)) // '/home' -> 'home', otherwise error
+          document.router.updatePageLinks()
         } else {
           throw new Error('Page loading error')
         }
