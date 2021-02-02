ee3ff7ce9d9c3114ce59c418dc08ff598e80c92a
edikxl <edik24032002@gmail.com>
2021-02-02 19:30:29 +0200

    Rewrote router without external modules

--- app/router.js
+++ app/router.js
@@ -1,6 +1,27 @@
-import 'https://unpkg.com/navigo@8.3.2/lib/navigo.min.js'
-// import Navigo from 'https://unpkg.com/navigo@7.1.2/lib/navigo.es.js'
-/* global Navigo */
+/* globals history */
+
+export class Router {
+  constructor (root) {
+    this.root = root
+    this.onnavigation = null
+
+    window.addEventListener('popstate', this.listener.bind(this))
+  }
+
+  listener (event) {
+    document.router.onnavigation(document.location.pathname)
+  }
+
+  navigate (URL) {
+    const state = {}
+    const title = ''
+
+    history.pushState(state, title, URL)
+    this.onnavigation(URL)
+  }
+}
+
+/*
 
 const root = '/'
 const useHash = false
@@ -10,6 +31,7 @@ const pageViewElement = document.getElementsByTagName('content-layer')[0]
 document.router = new Navigo(root, useHash)
 document.router
   .on('*', () => {
+    // document.view.update(URL)
     let pathname = document.location.pathname
     console.log(document.location, pathname) // Ya manal etor router, chto-to s nim ne tak
 
@@ -21,3 +43,5 @@ document.router
     pageViewElement.changePageTo(URL, defaultPage)
   })
   .resolve()
+
+*/
