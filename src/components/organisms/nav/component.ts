0f7daed65ad9644ca374ef28e5a1745da1993a33
edvein-rin <edveinrin@gmail.com>
2021-06-25 11:41:42 +0300

    Working Nav

--- src/components/organisms/nav/component.ts
+++ src/components/organisms/nav/component.ts
@@ -1,72 +1,110 @@
 import LinkAtom from 'Atoms/link/component'
 import Component from 'Components/component'
-import {Tree, Fold, PageTree} from 'Scripts/page-tree'
-
-/* Do I need these ? */
-const querySelectorAllDeep = require('query-selector-shadow-dom').querySelectorAllDeep
-const querySelectorDeep = require('query-selector-shadow-dom').querySelectorDeep
+import {Tree, Page, Fold, PageTree} from 'Scripts/page-tree'
 
 
 @Component.load(null, require('./styles.sass'))
 class NavOrganism extends Component {
   navItems: HTMLElement[] = []
+  pages: HTMLElement[] = []
+  folds: HTMLElement[] = []
+  activeNavItem: HTMLElement = undefined
 
 
   connectedCallback () {
     this.buildFromTree(this.shadow.querySelector('nav'), PageTree.tree)
-
-    /* TODO Click procession
-    const navItems = querySelectorAllDeep('m-nav-item')
-
-    navItems.forEach((navItem: HTMLElement) => {
-      navItem.addEventListener('click', (e) => {
-        const isLinkToSmth = querySelectorDeep('a-link', navItem).hasAttribute('to')
-        const isAnchor = navItem.getAttribute('anchor') !== null
-        
-        if (isLinkToSmth && !isAnchor) {
-          navItems.forEach((otherNavItem: HTMLElement) => {
-            otherNavItem.removeAttribute('active')
-          })
-          navItem.setAttribute('active', '')
-        }
-
-        e.preventDefault()
-        e.stopPropagation()
-      })
-    })
-    */
   }
 
   buildFromTree (root: HTMLElement, tree: Tree) {
     tree.forEach((treeElement) => {
       const isFold = treeElement.hasOwnProperty('children')
-      const element = document.createElement('div')
-      element.className = isFold ? 'fold' : 'page'
 
-      const title = document.createElement('div')
-      title.className = 'title'
+      const navItem = this.buildNavItem(isFold, treeElement)
 
-      const link = document.createElement('a-link') as LinkAtom
-      link.setAttribute('block', '')
+      root.appendChild(navItem)
+
+      this.navItems.push(navItem)
+      if (isFold) {
+        this.folds.push(navItem)
+      } else {
+        this.pages.push(navItem)
+      }
+    })
+  }
+
+  buildNavItem (isFold: boolean, treeElement: Page|Fold) {
+    const navItem = document.createElement('div')
+    navItem.classList.add(isFold ? 'fold' : 'page')
+
+    const title = this.buildNavItemTitle(isFold, treeElement)
+    navItem.appendChild(title)
+
+    if (isFold) {
+      title.addEventListener('click', (_) => {
+        if (navItem.classList.contains('open')) {
+          navItem.classList.remove('open')
+        } else {
+          navItem.classList.add('open')
+        }
+      })
+    } else {
+      title.addEventListener('click', (_) => {
+        const isMobile = window.matchMedia('(max-width: 992px)').matches // TODO Remove magic number
+  
+        if (isMobile) {
+          document.documentElement.setAttribute('data-menu-open', 'false')
+        }
+      })
+    }
+    
+    if (isFold) {
+      const accordeon = this.buildNavItemAccordeon.bind(this)(treeElement)
+      navItem.appendChild(accordeon)
+    }
+
+    if (!isFold) {
+      navItem.addEventListener('click', (e) => {
+        this.activeNavItem?.classList.remove('active')
+        this.activeNavItem = navItem
+        navItem.classList.add('active')
+
+        e.preventDefault()
+        e.stopPropagation()
+      })
+    }
+
+    return navItem
+  }
+
+  buildNavItemTitle (isFold: boolean, treeElement: Page|Fold): HTMLElement {
+    const title = document.createElement('div')
+    title.classList.add('title')
+
+    const link = this.buildTitleLink(isFold, treeElement)
+    title.appendChild(link)
+
+    return title
+  }
+
+  buildTitleLink (isFold: boolean, treeElement: Page|Fold): HTMLElement {
+    const link = document.createElement('a-link') as LinkAtom
+    link.setAttribute('block', '')
+    if (!isFold) {
       const path = PageTree.buildPathFromName(PageTree.tree, treeElement.name)
       link.setAttribute('to', path)
-      link.innerHTML = treeElement.title
-      link.update()
+    }
+    link.innerHTML = treeElement.title
+    link.update()
 
-      title.appendChild(link)
-      element.appendChild(title)
+    return link
+  }
 
-      if (isFold) {
-        const accordeon = document.createElement('div')
-        accordeon.className = 'accordeon'
-        this.buildFromTree(accordeon, (<Fold>treeElement).children)
+  buildNavItemAccordeon (treeElement: Page|Fold): HTMLElement {
+    const accordeon = document.createElement('div')
+    accordeon.classList.add('accordeon')
+    this.buildFromTree(accordeon, (<Fold>treeElement).children)
 
-        element.appendChild(accordeon)
-      }
-
-      this.navItems.push(element)
-      root.appendChild(element)
-    })
+    return accordeon
   }
 
   render () {
